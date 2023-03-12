cc64a236ba42b3438b566671a4f89278cb51ad17
Adil S <forever@nzironeed.ru>
2023-03-12 21:45:09 +0500

    made first task

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    item = Item('Samsa', 100000.0, 10)
+    return item
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 1000000.0
+
+
+def test_apply_discount(item):
+    assert item.apply_discount() == 100000.0
